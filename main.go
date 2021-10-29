b55230bf2683af6069e073f027ff92fb70e56b57
fwd_tzehwu.lay_usr-at-708677922150 <tzehwu.lay@fwd.com>
2021-10-29 15:05:10 +0800

    Remove duplicate code

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/laytzehwu/poc-go-lang/users"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,50 +22,10 @@ func main() {
 			"message": "pong pong",
 		})
 	})
-	// usersRouter := users.Router{
-	// 	AhLayGinEngine: router,
-	// }
-	// usersRouter.RouterInit()
-
-	userGroup := router.Group("/user")
-	{
-		userGroup.GET("", func(c *gin.Context) {
-			name := c.DefaultQuery("name", "Guest")
-			fmt.Printf("User %s is accessing\n", name)
-			c.JSON(200, gin.H{
-				"message": fmt.Sprintf("Welcome %s", name),
-			})
-		})
-		specUser := userGroup.Group("/:name")
-		{
-			specUser.GET("", func(c *gin.Context) {
-				name := c.Param("name")
-				fmt.Printf("User %s is accessing\n", name)
-				c.JSON(200, gin.H{
-					"message": fmt.Sprintf("Welcome %s", name),
-				})
-			})
-			specUserAction := specUser.Group("/*action")
-			{
-				specUserAction.GET("", func(c *gin.Context) {
-					name := c.Param("name")
-					action := c.Param("action")[1:]
-					fmt.Printf("User %s is %sing\n", name, action)
-					c.String(http.StatusOK, fmt.Sprintf("%s does %s", name, action))
-				})
-				specUserAction.POST("", func(c *gin.Context) {
-					name := c.Param("name")
-					action := c.Param("action")[1:]
-					reqPath := c.FullPath()
-					fmt.Printf("Receiving user action %s to do %sing via below path:\n\t%s\n", name, action, reqPath)
-					c.JSON(200, gin.H{
-						"message": fmt.Sprintf("Hi %s, we received your action: %s", name, action),
-					})
-				})
-			}
-		}
-
+	usersRouter := users.Router{
+		AhLayGinEngine: router,
 	}
+	usersRouter.RouterInit()
 
 	router.GET("/user-action", func(c *gin.Context) {
 		name := c.Query("name")
